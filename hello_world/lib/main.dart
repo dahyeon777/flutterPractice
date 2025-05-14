c0922028ba8574deb9f4ddcabfb705eec3dc317d
dahyeon777 <daaaa0727@gmail.com>
2025-05-14 16:25:16 +0900

    asset image

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -3,21 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   // 플러터 앱을 실행한다.
   runApp(
-    //MaterialApp은 항상 최상위에 위치한다.
-    //Scaffold는 바로 아래에 위치한다.
-    //위젯 -> Widget
     MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            'Code Factory',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-        ),
+        backgroundColor: Colors.blue,
+        body: Image.asset('asset/img/logo.png'),
       ),
     ),
   );
