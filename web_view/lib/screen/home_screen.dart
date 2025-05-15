f65fd9874f5e60ceb14183f7f5c7eb99ccb05ad4
dahyeon777 <daaaa0727@gmail.com>
2025-05-15 21:06:44 +0900

    web_view

---
+++ web_view/lib/screen/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
+class HomeScreen extends StatelessWidget {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeUrl);
+
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text(
+          'Code Factory',
+          style: TextStyle(
+            color: Colors.white, // 제목 텍스트 색상 변경
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: (){
+              controller.loadRequest(homeUrl);
+            },
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+        ],
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
