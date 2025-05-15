cf61a61f8abe81e3066925415b93178108e12d69
dahyeon777 <daaaa0727@gmail.com>
2025-05-15 19:42:31 +0900

    Row & Column end

--- row_and_column/lib/screen/home_screen.dart
+++ row_and_column/lib/screen/home_screen.dart
@@ -8,19 +8,50 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          color: Colors.black,
-          child: Column(
-            children: colors
-                .map(
-                  (e) => Container(
-                    height: 50.0,
-                    width: 50.0,
-                    color: e,
-                  ),
-                )
-                .toList(),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: colors
+                  .map((e) => Container(
+                        height: 50.0,
+                        width: 50.0,
+                        color: e,
+                      ))
+                  .toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  color: Colors.orange,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: colors
+                  .map((e) => Container(
+                height: 50.0,
+                width: 50.0,
+                color: e,
+              ))
+                  .toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
