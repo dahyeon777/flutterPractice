4edea151954361d0fd1243807cdcc6c854fe8b14
dahyeon777 <daaaa0727@gmail.com>
2025-05-16 00:45:52 +0900

    PageView practice

---
+++ image_carousel/lib/screen/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        children: [1, 2, 3, 4, 5]
+            .map(
+              (e) => Image.asset(
+                'asset/img/image_$e.jpeg',
+                fit: BoxFit.cover,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
