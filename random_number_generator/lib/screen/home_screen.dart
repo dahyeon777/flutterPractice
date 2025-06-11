acb464092f3cbad9bbf4fa18bacb8bac4a6016cc
dahyeon777 <daaaa0727@gmail.com>
2025-06-11 10:20:05 +0900

    'layout'

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -6,7 +6,40 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            ///제목과 아이콘 버튼이 있는곳
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('제목'),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.settings,
+                  ),
+                ),
+              ],
+            ),
+
+            ///숫자가 있는곳
+            Expanded(
+              child: Text('''
+              123
+              456
+              789
+              '''),
+            ),
+
+            ///버튼이 있는곳
+            ElevatedButton(
+              onPressed: (){},
+              child: Text('생성하기!'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
