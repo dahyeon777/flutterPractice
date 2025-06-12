c876e6587f2a28f5553b70a22c674bc1d1b8aa9a
dahyeon777 <daaaa0727@gmail.com>
2025-06-13 02:49:52 +0900

    state

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:random_number_generator/constant/color.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<int> numbers = [
+    123,
+    456,
+    789,
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +31,22 @@ class HomeScreen extends StatelessWidget {
               _Header(),
 
               ///숫자가 있는곳
-              _Body(),
+              _Body(
+                numbers: numbers,
+              ),
 
               ///버튼이 있는곳
-              _Footer(),
+              _Footer(
+                onPressed: (){
+                  setState(() {
+                    numbers = [
+                      999,
+                      888,
+                      777
+                    ];
+                  });;
+                },
+              ),
             ],
           ),
         ),
@@ -61,18 +84,19 @@ class _Header extends StatelessWidget {
 }
 
 class _Body extends StatelessWidget {
-  const _Body({super.key});
+  final List<int> numbers;
+
+  const _Body({
+    required this.numbers,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          123,
-          456,
-          789,
-        ]
+        children: numbers
             .map((e) => e.toString().split(''))
             .map(
               (e) => Row(
@@ -94,12 +118,17 @@ class _Body extends StatelessWidget {
 }
 
 class _Footer extends StatelessWidget {
-  const _Footer({super.key});
+  final VoidCallback onPressed;
+
+  const _Footer({
+    required this.onPressed,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: ElevatedButton.styleFrom(
         backgroundColor: redColor,
         foregroundColor: Colors.white,
